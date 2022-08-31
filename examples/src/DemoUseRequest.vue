d4267ff8e0e91826d96b4d4c49005f522c25c0ba
Yoyeo98137 <1695782408@qq.com>
2022-08-31 21:24:43 +0800

    feat(useRequest): demo —— 快速上手

---
+++ examples/src/DemoUseRequest.vue
@@ -0,0 +1,79 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { ElMessage } from 'element-plus';
+import { useRequest } from '@/packages';
+
+// ---------------------- 默认用法
+function getUsername(): Promise<string> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('yeo');
+    }, 1280);
+  });
+}
+const { data, loading } = useRequest(getUsername, {
+  onSuccess: (data) => {
+    console.log('🏄 ---- onSuccess ---- data', data);
+  },
+});
+
+// ---------------------- 手动触发
+function changeUsername(username: string): Promise<{ success: boolean }> {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve({ success: true });
+    }, 1000);
+  });
+}
+const changeName = ref('');
+const { loading: mauLoading, run } = useRequest(changeUsername, {
+  manual: true,
+  onSuccess: (result, params) => {
+    if (result.success) {
+      changeName.value = '';
+      ElMessage({
+        message: `The username was changed to "${params[0]}" !`,
+        type: 'success',
+      });
+    }
+  },
+});
+</script>
+
+<template>
+  <ElCard shadow="never">
+    <div>Welcome to vhooks!</div>
+  </ElCard>
+
+  <ElCard shadow="never">
+    <div class="mb12">默认用法</div>
+    <ElCard v-loading="loading">
+      <div>Username: {{ data }}</div>
+    </ElCard>
+  </ElCard>
+
+  <ElCard shadow="never">
+    <div class="mb12">手动触发</div>
+    <ElCard>
+      <ElRow>
+        <ElCol :span="6">
+          <el-input v-model="changeName" placeholder="Please enter username" />
+        </ElCol>
+        <ElCol :span="2" style="margin-left: 16px">
+          <el-button :loading="mauLoading" @click="run(changeName)">{{
+            mauLoading ? 'Loading...' : 'Edit'
+          }}</el-button>
+        </ElCol>
+      </ElRow>
+    </ElCard>
+  </ElCard>
+</template>
+
+<style scoped>
+.el-card + .el-card {
+  margin-top: 28px;
+}
+.mb12 {
+  margin-bottom: 12px;
+}
+</style>
