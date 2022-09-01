13313372449286adf631f17f9d724f3d3388a9ba
Yoyeo98137 <1695782408@qq.com>
2022-09-01 22:20:26 +0800

    feat: 补充工具类型 —— Timeout、Nullable

--- packages/utils/types.ts
+++ packages/utils/types.ts
@@ -5,3 +5,7 @@ export type UnRef<T> = T extends Ref<infer V> ? V : T;
 export type UnWrapRefObject<T> = {
   [P in keyof T]: UnRef<T[P]>;
 };
+
+export type Timeout = ReturnType<typeof setTimeout>;
+
+export type Nullable<T> = T | null;
