d0b919b89e2645a870fc143b82afd1b850712278
Yoyeo98137 <1695782408@qq.com>
2022-09-01 22:20:58 +0800

    feat(useRequest): add plugin useLoadingDelayPlugin

--- packages/useRequest/src/plugins/useLoadingDelayPlugin.ts
+++ packages/useRequest/src/plugins/useLoadingDelayPlugin.ts
@@ -1,7 +1,38 @@
-export const useLoadingDelayPlugin = () => {
+import type { PluginUnRefs } from '../types';
+import type { Nullable, Timeout } from '../../../utils/types';
+
+export const useLoadingDelayPlugin: PluginUnRefs<any, any[]> = (
+  fetchInstance,
+  { loadingDelay }
+) => {
+  if (!loadingDelay) return {};
+
+  let delayTimer: Nullable<Timeout> = null;
+  const cancelTimeout = () => {
+    if (delayTimer) {
+      clearTimeout(delayTimer);
+    }
+  };
+
   return {
-    onSuccess: () => {
-      console.log('🏄 ---- useLoadingDelayPlugin ---- onSuccess');
+    onBefore: () => {
+      cancelTimeout();
+
+      delayTimer = setTimeout(() => {
+        fetchInstance.setState({
+          loading: true,
+        });
+      }, loadingDelay);
+
+      return {
+        loading: false,
+      };
+    },
+    onFinally: () => {
+      cancelTimeout();
+    },
+    onCancel: () => {
+      cancelTimeout();
     },
   };
 };
