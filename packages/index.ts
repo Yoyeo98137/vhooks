bde9ad789d328d54e35b0bc0ce982caa95a8e8d0
Yoyeo98137 <1695782408@qq.com>
2022-09-04 11:12:49 +0800

    refactor: 清理文件

--- packages/index.ts
+++ packages/index.ts
@@ -1,5 +1,3 @@
 // 重新导出 导入模块的所有导出
 
 export * from './useRequest';
-// export * from './usePagination';
-// export * from './useTable';
--- packages/utils/lodash/index.ts
+++
@@ -1,5 +0,0 @@
-export { default as deepClone } from './clone';
-export { default as debounce } from './debounce';
-export { default as merge } from './merge';
-export { default as throttle } from './throttle';
-export { default as throttlePlus } from './throttlePlus';
